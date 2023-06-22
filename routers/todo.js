8983db71d20aa3110d6bd75698e1b058268f83ec
thehassanshaikh <thehasanshaikh@gmail.com>
2023-06-22 19:05:01 +0530

    added  the methods to the end points

--- routers/todo.js
+++ routers/todo.js
@@ -1,37 +1,40 @@
+// routes/todo.js
 const express = require("express");
-
 const router = express.Router();
+
+const {
+    getAllTodo,
+    postCreateTodo,
+    putUpdateTodo,
+    deleteTodo,
+} = require("../controllers/todo");
 
 /**
  * @route GET api/todo
  * @description get all todo
  * @access public
  */
-
-router.get("/");
+router.get("/", getAllTodo);
 
 /**
  * @route POST api/todo
  * @description add a new todo
  * @access public
  */
-
-router.post("/");
+router.post("/", postCreateTodo);
 
 /**
  * @route PUT api/todo/:id
  * @description update todo
  * @access public
  */
-
-router.put("/:id");
+router.put("/:id", putUpdateTodo);
 
 /**
  * @route DELETE api/todo/:id
  * @description delete todo
  * @access public
  */
+router.delete("/:id", deleteTodo);
 
-router.delete("/:id");
-
-module.exports(router);
+module.exports = router;
